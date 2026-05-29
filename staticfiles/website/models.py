274086401ba870162fc723be298c64a06b8c0393
vaishaliahir <chavdavaishali639@gmail.com>
2026-05-29 10:07:48 +0530

    fix static files path

---
+++ staticfiles/website/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+# Create your models here.
